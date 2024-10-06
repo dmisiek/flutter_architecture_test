c0eb41b418bbbca1c7a5f044d7864e5216b2a34b
dmisiek <dominikmisiek02@gmail.com>
2024-10-06 20:03:25 +0200

    Generated flutter app with very_good_cli

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/dinner/time/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.dinner_time
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:dinner_time/app/app.dart';
+import 'package:dinner_time/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
